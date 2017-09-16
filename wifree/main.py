32714d927f7a80c7aa622444eca7be2cd1977305
Yogeshwar Velingker <yvelingk@mit.edu>
2017-09-16 19:05:14 -0400

    Parse latitude, longitude

--- wifree/main.py
+++ wifree/main.py
@@ -4,12 +4,27 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+def parse_body(body):
+    arr = body.split(',')
+    if len(arr) != 2:
+        raise ValueError
+    lat = float(arr[0])
+    lon = float(arr[1])
+    if lat < -90 or lat > 90:
+        raise ValueError
+    if lon < -180 or lon > 180:
+        raise ValueError
+    return str(lat) + ',' + str(lon)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-    # incomingnumber = request.values.get('From', None)
     print('received message')
     body = request.values.get('Body', None)
+    coord = (None, None)
+    try:
+        coord = parse_body(body)
+    except:
+        return
 
     resp = MessagingResponse()
     resp.message(body)
