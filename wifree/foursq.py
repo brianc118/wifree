9e8006ef1daf3350c8bb052edb3640e7d49801f1
Yogeshwar Velingker <yvelingk@mit.edu>
2017-09-16 23:06:34 -0400

    Change 'wifi' to 'Free Wifi'

--- wifree/foursq.py
+++ wifree/foursq.py
@@ -4,7 +4,7 @@ client = foursquare.Foursquare(client_id='4SQ_ID', client_secret='4SQ_SECRET')
 
 def get_wificoord(location):
     # venue search see https://developer.foursquare.com/docs/venues/search
-    results = client.venues.explore(params={'query': 'wifi', 'll': location})
+    results = client.venues.explore(params={'query': 'Free Wifi', 'll': location})
     topresult = results[u'groups'][0][u'items'][0][u'venue']
     topresultname = topresult[u'name']
     topresultloc = topresult[u'location']
