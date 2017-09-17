18c885ef7e691d91919c4d246b578f42883edb8a
brianc118 <brianchen118@gmail.com>
2017-09-17 09:42:42 -0400

    syntax error fix

--- wifree/maps.py
+++ wifree/maps.py
@@ -48,7 +48,7 @@ def get_coord(result):
 
 def get_wificoord(location, mode):
     results = []
-    for i in range (floor(log10(rad_min,rad_max)))
+    for i in range (floor(log10(rad_min,rad_max))):
         try:
             radius = i * rad_min  
             rawresults = client.venues.explore(params={'query': 'Wifi', 'll': location, 'limit': 50, 'radius', radius})
